1b8b6031cf988da5331f9915d22f8df80803f78b
PetarPaunov <petarp.2000@abv.bg>
2022-07-22 13:23:58 +0300

    Revert "Add project GUI-Tic-Tac-Toe"
    This reverts commit e040577901bbb084b477d90dbffbfb7bdc67924b.

--- TicTacToeForm.Designer.cs
+++ TicTacToeForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TicTacToe
 {
-    partial class formTicTacToe
+    partial class TicTacToeForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,60 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panelButtons = new System.Windows.Forms.Panel();
-            this.buttonPlayer = new System.Windows.Forms.Button();
-            this.labelPlayer = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // panelButtons
-            // 
-            this.panelButtons.Location = new System.Drawing.Point(35, 31);
-            this.panelButtons.Margin = new System.Windows.Forms.Padding(4);
-            this.panelButtons.Name = "panelButtons";
-            this.panelButtons.Size = new System.Drawing.Size(600, 554);
-            this.panelButtons.TabIndex = 0;
-            // 
-            // buttonPlayer
-            // 
-            this.buttonPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.buttonPlayer.Location = new System.Drawing.Point(664, 100);
-            this.buttonPlayer.Margin = new System.Windows.Forms.Padding(4);
-            this.buttonPlayer.Name = "buttonPlayer";
-            this.buttonPlayer.Size = new System.Drawing.Size(133, 123);
-            this.buttonPlayer.TabIndex = 1;
-            this.buttonPlayer.Text = "X";
-            this.buttonPlayer.UseVisualStyleBackColor = true;
-            // 
-            // labelPlayer
-            // 
-            this.labelPlayer.AutoSize = true;
-            this.labelPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.labelPlayer.Location = new System.Drawing.Point(664, 65);
-            this.labelPlayer.Margin = new System.Windows.Forms.Padding(4, 0, 3, 0);
-            this.labelPlayer.Name = "labelPlayer";
-            this.labelPlayer.Size = new System.Drawing.Size(97, 31);
-            this.labelPlayer.TabIndex = 2;
-            this.labelPlayer.Text = "Player";
-            // 
-            // formTicTacToe
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(832, 617);
-            this.Controls.Add(this.labelPlayer);
-            this.Controls.Add(this.buttonPlayer);
-            this.Controls.Add(this.panelButtons);
-            this.Name = "formTicTacToe";
-            this.Text = "Tic Tac Toe";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
         }
 
         #endregion
-
-        private Panel panelButtons;
-        private Button buttonPlayer;
-        private Label labelPlayer;
     }
 }
